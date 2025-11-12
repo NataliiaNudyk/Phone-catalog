2afc3eb69487ee03bc2fc7e892ee4d959ed0540e
Nataliia Nudyk <nataliianudyk@gmail.com>
2025-11-12 15:17:33 +0200

    change link for Order now button

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import { useContext } from 'react';
 import './App.scss';
+import { Footer } from './components/footer';
+import { Header } from './components/header';
+import { Outlet } from 'react-router-dom';
+import { ProductContext } from './context/ProductContext';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
+export const App = () => {
+  const { openMenu } = useContext(ProductContext);
 
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
-export const App: React.FC = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="grid-container">
+      <Header />
+      {!openMenu && (
+        <>
+          <main className="main">
+            <Outlet />
+          </main>
+          <Footer />
+        </>
+      )}
     </div>
   );
 };
